187c7ef0a6bdbfa82a47197f0ba6406092c9551d
ruan.apolinario <ruan.apolinario@ES-004947.saude.gov>
2021-12-20 16:05:11 -0300

    case

--- Projeto1/src/curso1/java/curso1.java
+++ Projeto1/src/curso1/java/curso1.java
@@ -4,23 +4,23 @@ public class curso1 {
 
 	public static void main(String[] args) {
 		
-		int n1 = 90;
-		int n2 = 80;
-		int n3 = 30;
-		int n4 = 80;
-		double media = 0;
+		int n1 = 60;
+		int n2 = 60;
+		int n3 = 60;
+		int n4 = 60;
+		int media = 0;
 		
 		media = (n1 + n2 + n3 + n4) / 4;
 		
-		/*Operações lógicas aninhadas. operações dentro de operações*/
-		if (media >= 50) {
-			if (media >= 70) {
-				System.out.println("Aluno aprovado direto");
-			}else {
-				System.out.println("Aluno em recuperação");
-			}
-		} else {
-			System.out.println("Aluno reprovado");
-		}
+	/*Switch Case, operações EXATAS*/
+		
+	switch (media) {
+	case 60:
+		System.out.println("aprovado");
+		break;
+
+	default:System.out.println("Outro valor: " + media);
+		break;
+	}
 	}
 }
