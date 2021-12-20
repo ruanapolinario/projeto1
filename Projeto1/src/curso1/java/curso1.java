281aa4cfcff2c33a9d8d008528bcfd69b993aa7e
ruan.apolinario <ruan.apolinario@ES-004947.saude.gov>
2021-12-20 15:07:25 -0300

    tste

--- Projeto1/src/curso1/java/curso1.java
+++ Projeto1/src/curso1/java/curso1.java
@@ -5,16 +5,7 @@ public class curso1 {
 	public static void main(String[] args) {
 		
 		int maiorIdade = 18;
-		int idosoIdade = 65;
-		int jovemIdade = 16;
-		int criançaIdade = 7;
-		
-		String cpf = "057.876.447-43";
-		
-		System.out.println("Maior idade é = " + maiorIdade);
-
-		System.out.println("CPF é = " + cpf);
-		
-		
+	
+		/*wertwertwert*/
 	}
 }
