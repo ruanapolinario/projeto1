970d990571732095dd382a740f7de2a258d122de
ruan.apolinario <ruan.apolinario@ES-004947.saude.gov>
2021-12-20 15:24:28 -0300

    aninhado

--- Projeto1/src/curso1/java/curso1.java
+++ Projeto1/src/curso1/java/curso1.java
@@ -4,8 +4,23 @@ public class curso1 {
 
 	public static void main(String[] args) {
 		
-		int maiorIdade = 18;
-	
-		/*wertwertwert*/
+		int n1 = 90;
+		int n2 = 80;
+		int n3 = 30;
+		int n4 = 80;
+		double media = 0;
+		
+		media = (n1 + n2 + n3 + n4) / 4;
+		
+		/*Operações lógicas aninhadas. operações dentro de operações*/
+		if (media >= 50) {
+			if (media >= 70) {
+				System.out.println("Aluno aprovado direto");
+			}else {
+				System.out.println("Aluno em recuperação");
+			}
+		} else {
+			System.out.println("Aluno reprovado");
+		}
 	}
 }
