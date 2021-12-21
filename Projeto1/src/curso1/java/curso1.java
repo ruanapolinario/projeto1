a1fad42eb6e039075c9619efb8af92b783ae57b4
ruan.apolinario <ruan.apolinario@ES-004947.saude.gov>
2021-12-21 14:00:15 -0300

    dowhile

--- Projeto1/src/curso1/java/curso1.java
+++ Projeto1/src/curso1/java/curso1.java
@@ -3,22 +3,27 @@ package curso1.java;
 public class curso1 {
 
 	public static void main(String[] args) {
-		
-		/*int n1 = 60;
-		int n2 = 60;
-		int n3 = 60;
-		int n4 = 60;
-		int media = 0;
-		
-		media = (n1 + n2 + n3 + n4) / 4;*/
-		
+
+		/*
+		 * int n1 = 60; int n2 = 60; int n3 = 60; int n4 = 60; int media = 0;
+		 * 
+		 * media = (n1 + n2 + n3 + n4) / 4;
+		 */
+
 		int num = 0;
-		
-	/*While*/
-		
+
+		/* While, verifica e dps executa o codigo */
+
 		while (num <= 10) {
 			System.out.println("numero atual " + num);
-			num ++;
+			num++;
 		}
-		}
+
+		int num2 = 0;
+		do {
+			System.out.println("num2 é " + num2);
+			num2++;
+		} while (num2 <= 60);
+
+	}
 }
