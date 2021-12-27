b7d35ac550ae96866facbac7cceb3d5723d000ad
ruan.apolinario <ruan.apolinario@ES-004947.saude.gov>
2021-12-27 11:27:39 -0300

    modulo resto %

--- Projeto1/src/curso1/java/curso1.java
+++ Projeto1/src/curso1/java/curso1.java
@@ -4,20 +4,12 @@ public class curso1 {
 
 	public static void main(String[] args) {
 
-		/*
-		 * int n1 = 60; int n2 = 60; int n3 = 60; int n4 = 60; int media = 0;
-		 * 
-		 * media = (n1 + n2 + n3 + n4) / 4;
-		 */
-
-		for (int num = 0; num <= 10; num++) {
-			if (num == 2 || num == 4 || num == 7 || num == 8) {
-				System.out.println("Encontrei o valor: " + num);
-				continue;
-			}
-
-			System.out.println("processando laço de repetição");
-		}
-
+		/*Modulo resto da divisao: %*/
+		
+		double num1 = 9;
+		double pessoas = 2;
+		
+		double resto = num1 % pessoas;
+		System.out.println("Sobraram >: " + resto);
 	}
 }
