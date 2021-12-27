64df95d3f7517830ce6cee5c03a13f94bff3b51c
ruan.apolinario <ruan.apolinario@ES-004947.saude.gov>
2021-12-27 15:11:56 -0300

    inserindo.dados

--- Projeto1/src/curso1/java/curso1.java
+++ Projeto1/src/curso1/java/curso1.java
@@ -1,15 +1,28 @@
 package curso1.java;
 
+import javax.swing.JOptionPane;
+
 public class curso1 {
 
 	public static void main(String[] args) {
 
-		/*Modulo resto da divisao: %*/
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros:");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas: ");
+
+		/* Transformar string em double */
+		double carroNum = Double.parseDouble(carros);
+		double pessoaNum = Double.parseDouble(pessoas);
+
+		/* Calcula a divisao e depois converte para inteiro */
+		int cada = (int) (carroNum / pessoaNum);
+
+		double resto = carroNum % pessoaNum;
+
+		JOptionPane.showMessageDialog(null, "Cada pessoa ficará com " + cada + " e restará " + resto);
 		
-		double num1 = 9;
-		double pessoas = 2;
-		
-		double resto = num1 % pessoas;
-		System.out.println("Sobraram >: " + resto);
+		/*
+		 * System.out.println("Cada pessoa ficará com " + cada + " e restará " + resto);
+		 */
+
 	}
 }
