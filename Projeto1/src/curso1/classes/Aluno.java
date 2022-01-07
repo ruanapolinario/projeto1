9398d4b1894bf434904f571a0db6f151b443941a
ruan.apolinario <ruan.apolinario@ES-004947.saude.gov>
2022-01-07 14:38:08 -0300

    objeto

---
+++ Projeto1/src/curso1/classes/Aluno.java
@@ -0,0 +1,18 @@
+package curso1.classes;
+
+/*Essa é classe/objeto */
+public class Aluno {
+
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String serieMatriculado;
+	
+	/*Construtor de objeto*/
+	
+}
